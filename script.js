9ec979580a4adde746b70e21ca68b3ceefdb243b
KamilaAzul <>
2023-11-14 14:06:25 +0000

    Update congratulations message

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Selecting DOM elements
+// DOM elements
 const cards = document.querySelectorAll('.memory-card');
 const movesCount = document.querySelector("#moves");
 const scoreDisplay = document.querySelector("#score");
 const winMessage = document.querySelector("#win-message");
+
 
 // Game state variables
 let hasFlippedCard = false;
@@ -10,13 +11,16 @@ let lockBoard = false;
 let firstCard, secondCard;
 let score = 0;
 
+
 // Event listener function for card click
 function flipCard() {
     // Check if the board is locked or the same card is clicked
     if (lockBoard || this === firstCard) return;
 
+
     // Add 'flip' class to the clicked card
     this.classList.add('flip');
+
 
     if (!hasFlippedCard) {
         // First card click
@@ -26,9 +30,10 @@ function flipCard() {
         // Second card click
         secondCard = this;
 
+
         // Check if the cards match
         if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            // The cards match --> disable cards, update score, and reset the board
+            // If the cards match disable cards, update score, and reset the board
             firstCard.removeEventListener('click', flipCard);
             secondCard.removeEventListener('click', flipCard);
             resetBoard();
@@ -36,30 +41,36 @@ function flipCard() {
             score++;
             scoreDisplay.textContent = score;
 
+
             if (document.querySelectorAll('.flip').length === cards.length) {
-                // Display congratulations message and game stats
+                // Display congratulations message and game results
                 if (winMessage) {
                     winMessage.classList.remove('hidden');
                     winMessage.innerHTML = `
-                        <p>Congratulations! You've matched all the cards!</p>
-                        <p>Moves: <span id="moves">${movesCount.innerHTML}</span></p>
-                        <p>Score: <span id="score">${score}</span></p>
+                    <h1>Congratulations!</h1>
+                    <h1>You have paired all cards.</h1>
+                    <br>
+                    <h3>Here's your result:</h3>
+                    <br>
+                    <h1>Moves: <span id="moves">${movesCount.innerHTML}</span></h1>
+                    <h1>Score: <span id="score">${score}</span></h1>
                     `;
                 }
-            
-                // Redirect to congratulation.html after a delay (e.g., 2 seconds)
+           
+                // Redirect to congratulation.html after 2 seconds
                 setTimeout(() => {
                     const queryString = `moves=${movesCount.innerHTML}&score=${score}`;
                     window.location.href = `congratulation.html?${queryString}`;
                 }, 2000);
             }
         } else {
-            // The cards don't match --> unflip cards, add animation, and reset the board
+            // If the cards don't match unflip cards, add animation, and reset the board
             lockBoard = true;
             setTimeout(() => {
                 firstCard.classList.remove('flip');
                 secondCard.classList.remove('flip');
             }, 1500);
+
 
             // Add 'shake' animation when cards don't match
             setTimeout(() => {
@@ -68,6 +79,7 @@ function flipCard() {
                     secondCard.classList.add("shake");
                 }
             }, 1000);
+
 
             // Remove 'shake' and 'flip' classes after animation and reset the board
             setTimeout(() => {
@@ -79,16 +91,19 @@ function flipCard() {
                 }
             }, 1500);
 
+
             // Count the move
             movesCounter();
         }
     }
 }
 
+
 // Counting each move of the player
 function movesCounter() {
     movesCount.innerHTML++;
 }
+
 
 // Reset the board state
 function resetBoard() {
@@ -98,6 +113,7 @@ function resetBoard() {
     lockBoard = false;
 }
 
+
 // Immediately Invoked Function Expression (IIFE) for shuffling cards
 (function shuffle() {
     cards.forEach(function (card) {
@@ -106,7 +122,10 @@ function resetBoard() {
     });
 })();
 
+
 // Add event listeners to cards
 cards.forEach(function (card) {
     card.addEventListener('click', flipCard);
 });
+
+
